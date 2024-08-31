0c941b7b2274873bdde04260a54a6797f976bc96
Douglas Faria <hfsdouglas@gmail.com>
2024-08-31 00:20:41 -0300

    feat: add axios library, getting coins market on add screen

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,6 +1,32 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
+
+import { Loading } from "@/components/loading";
+
+import { getCoinsMarketData } from "@/services/get-market";
 
 export default function Add() {
+    const [coinsMarket, setCoinsMarket] = useState<any>({})
+    const [isGettingCoinsMarket, setIsGettingCoinsMarket] = useState(true)
+
+    async function getCoinsMarket() {
+        try {
+            const coins = await getCoinsMarketData()
+            setCoinsMarket(coins)
+            setIsGettingCoinsMarket(false)
+        } catch (error) {
+            Alert.alert("Erro!", "Houve um erro ao carregar as moedas")
+        }
+    }
+    
+    useEffect(() => {
+        getCoinsMarket()
+    }, [])
+
+    if (isGettingCoinsMarket) {
+        return <Loading />
+    }
+
     return (
         <View className="flex-1 justify-center items-center">
             <Text>Add Coin</Text>
