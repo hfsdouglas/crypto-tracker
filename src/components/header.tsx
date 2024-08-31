68f2ecbdb6ef754231dc37216f1998963c674d93
Douglas Faria <hfsdouglas@gmail.com>
2024-08-30 23:05:40 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function Header() {
+    return (
+        <View className="flex-row bg-blue-950 h-36 w-full absolute top-0 pt-16 px-5 pb-5 justify-between items-center">
+            <Text className="text-2xl font-extrabold text-white">Crypto Tracker</Text>
+            <View className="h-12 w-12 rounded-full bg-white" />
+        </View>
+    )
+}
