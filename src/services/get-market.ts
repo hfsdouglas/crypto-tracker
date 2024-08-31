aa647d0727e81a344bd14acd1969a3685e6415be
Douglas Faria <hfsdouglas@gmail.com>
2024-08-31 15:55:53 -0300

    feat: structuring api data

--- src/services/get-market.ts
+++ src/services/get-market.ts
@@ -1,13 +1,37 @@
 import { api } from "@/lib/api"
 
+export type Coin = {
+    id: string,
+    symbol: string,
+    name: string,
+    image: string,
+    current_price: number
+}
+
+async function getCoinsMarket() {
+    const response = await api.get('/coins/markets?vs_currency=usd&per_page=50')
+    return response
+}
 
 async function getCoinsMarketData() {
-    try {
-        const response = await api.get('/coins/markets?vs_currency=usd&per_page=50')
-        return response
-    } catch (error) {
-        throw error
-    }
+    const { data } = await getCoinsMarket()
+    const fields = ['id', 'name', 'symbol', 'image', 'current_price', 'price_change_percentage_24h']
+    let coinsData: Coin[] = []
+
+    data.map((coin: any) => {
+        let coinData:any = {}
+
+        for (const field in coin) {
+            if (fields.includes(field)) {
+                coinData[field] = coin[field]
+            }
+        }
+
+        coinsData.push(coinData)
+    })
+
+    return coinsData
 }
 
 export { getCoinsMarketData }
+
