aaaa6f896e900688d5d1518bb72c5c89976a3d7d
Douglas Faria <hfsdouglas@gmail.com>
2024-08-31 15:54:53 -0300

    feat: add status bar component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,11 @@
-import { View } from "react-native"
+import { StatusBar, View } from "react-native"
 import { Slot } from "expo-router"
 import "../styles/global.css"
 
 export default function Layout() {
     return (
         <View className="flex-1">
+            <StatusBar barStyle="light-content" />
             <Slot />
         </View>
     )
