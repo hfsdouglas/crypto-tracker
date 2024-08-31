8e11b1020bcf051077fec6a57fc33cdad8bbc53c
Douglas Faria <hfsdouglas@gmail.com>
2024-08-30 22:31:51 -0300

    feat: add tailwindcss configs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,11 @@
+import { View } from "react-native"
 import { Slot } from "expo-router"
+import "../styles/global.css"
 
 export default function Layout() {
     return (
-        <Slot />
+        <View className="flex-1">
+            <Slot />
+        </View>
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Home() {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View className="flex-1 justify-center items-center">
+            <Text className="font-bold text-2xl">Hello World</Text>
         </View>
     )
 }
