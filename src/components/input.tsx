a86abc2c067b2b4d78796a272b0f2bb354d8b141
Douglas Faria <hfsdouglas@gmail.com>
2024-09-02 21:16:20 -0300

    feat: add text input props to input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, TextInputProps, View } from "react-native";
 
-export function Input() {
+export function Input({...rest}: TextInputProps) {
     return (
         <View className="border-2 border-blue-950 flex-1 rounded-md">
-            <TextInput className="text-lg px-4 py-2 text-blue-950" placeholder="Digite a criptomoeda" />
+            <TextInput className="text-lg px-4 py-2 text-blue-950" placeholder="Digite a criptomoeda" {...rest} />
         </View>
     )
 }
