56ef731bca9c3dcc3ce6b66dcdb7349cdba17d6e
Douglas Faria <hfsdouglas@gmail.com>
2024-09-02 21:17:17 -0300

    feat: add search functions

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Alert, FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Keyboard, Text, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { Loading } from "@/components/loading";
@@ -12,6 +12,30 @@ import { Coin, getCoinsMarketData } from "@/services/get-market";
 export default function Add() {
     const [coinsMarket, setCoinsMarket] = useState<Coin[]>([])
     const [isGettingCoinsMarket, setIsGettingCoinsMarket] = useState(true)
+    const [search, setSearch] = useState('')
+
+    function handleSearchButton() {
+        Keyboard.dismiss()
+
+        let searchCoins: Coin[] = []
+
+        coinsMarket.map(item => {
+            const name = item.name.toLocaleLowerCase()
+            const searchString = search.toLocaleLowerCase()
+
+            if (name.match(searchString)) {
+                searchCoins.push(item)
+            }
+        })
+
+        setCoinsMarket(searchCoins)
+    }
+
+    useEffect(() => {
+        if (search.length === 0) {
+            getCoinsMarket()
+        }
+    }, [search])
 
     async function getCoinsMarket() {
         try {
@@ -34,8 +58,8 @@ export default function Add() {
     return (
         <View className="flex-1 p-3">
             <View className="flex-row mb-3">
-                <Input />
-                <Button className="ml-3">
+                <Input value={search} onChangeText={setSearch} />
+                <Button className="ml-3" onPress={handleSearchButton}>
                     <MaterialIcons name="search" color="#FFFFFF" size={26} />
                 </Button>
             </View>
