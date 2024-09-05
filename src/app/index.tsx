7fa448956bd610def9334b1514773b6ec4da0b8a
Douglas Faria <hfsdouglas@gmail.com>
2024-09-04 21:07:42 -0300

    feat: add remove function to coin card

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,27 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, Alert } from "react-native";
 import { router } from "expo-router"
+
 import { Header } from "@/components/header";
 import { AddButton } from "@/components/add-button";
-import { useCoinStore } from "@/stores/coin-store";
 import { CoinCard } from "@/components/coin-card";
+
+import { Coin } from "@/services/get-market";
+
+import { useCoinStore } from "@/stores/coin-store";
 
 export default function Home() {
     const { coins, remove } = useCoinStore()
+
+    function handleCardDeletePress (coin: Coin) {
+        Alert.alert(
+            "Atenção!", 
+            `Tem certeza que deseja remover ${coin.name}?`,
+            [
+                {text: "Não", onPress: () => {}, style: "cancel" },
+                {text: "Sim", onPress: () => remove(coin.id) },
+            ]
+        )
+    }
 
     return (
         <View className="flex-1 justify-center items-center">
@@ -17,7 +32,7 @@ export default function Home() {
                     data={coins}
                     keyExtractor={item => item.id}
                     renderItem={({item}) => (
-                        <CoinCard coin={item} />
+                        <CoinCard coin={item} onPress={() => handleCardDeletePress(item)}/>
                     )}
                 />
             </View>
