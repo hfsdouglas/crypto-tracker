a3f014c2055f6410ebecb245af95ef1be6b9a297
Douglas Faria <hfsdouglas@gmail.com>
2024-09-04 21:09:43 -0300

    feat: resizing height on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,8 @@ import { Text, View } from "react-native";
 
 export function Header() {
     return (
-        <View className="flex-row bg-blue-950 h-36 w-full pt-16 px-5 pb-5 justify-between items-center">
+        <View className="flex-row bg-blue-950 h-32 w-full pt-16 px-5 pb-5 justify-between items-center">
             <Text className="text-2xl font-extrabold text-white">Crypto Tracker</Text>
-            <View className="h-12 w-12 rounded-full bg-white" />
         </View>
     )
 }
