82ead7e9dde18efabfb7b06d907a60f0deae89c7
Douglas Faria <hfsdouglas@gmail.com>
2024-09-04 23:13:20 -0300

    feat: add translucent on status-bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,11 @@
-import { StatusBar, View } from "react-native"
+import { View, StatusBar } from "react-native"
 import { Slot } from "expo-router"
 import "../styles/global.css"
 
 export default function Layout() {
     return (
         <View className="flex-1 bg-slate-200">
-            <StatusBar barStyle="light-content" />
+            <StatusBar barStyle="light-content" translucent />
             <Slot />
         </View>
     )
